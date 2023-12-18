6ac1323fab2b1e95247fd41e7dff30da53f4cbfa
kyaloeric <erickyalo19@gmail.com>
2023-12-18 10:15:20 +0300

    modify backend

--- backend/src/database/tables/followers.sql
+++
@@ -1,9 +0,0 @@
--- USE Tweet
-
---   CREATE TABLE Follower (
---     follower_id VARCHAR(200) PRIMARY KEY,
---     follower_user_id VARCHAR(200),
---     following_user_id VARCHAR(200),
---     CONSTRAINT FK_Follower_User FOREIGN KEY (follower_user_id) REFERENCES Users(user_id),
---     CONSTRAINT FK_Following_User FOREIGN KEY (following_user_id) REFERENCES Users(user_id)
---   );
